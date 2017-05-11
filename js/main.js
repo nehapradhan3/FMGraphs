17a63dad6876d77ebf316a1f26305e079ccd1876
nehapradhan <nehapradhan_3@hotmail.com>
2017-05-11 18:00:25 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,149 @@
+var chartData = generateChartData();
+
+var chart = AmCharts.makeChart("chartdiv", {
+    "type": "serial",
+    "theme": "light",
+    "legend": {
+        "useGraphSettings": true
+    },
+    "dataProvider": chartData,
+    "synchronizeGrid":true,
+    "valueAxes": [{
+        "id":"v1",
+        "axisColor": "#FF6600",
+        "axisThickness": 2,
+        "axisAlpha": 1,
+        "position": "left"
+    }, {
+        "id":"v2",
+        "axisColor": "#FCD202",
+        "axisThickness": 2,
+        "axisAlpha": 1,
+        "position": "right"
+    }],
+    "graphs": [{
+        "valueAxis": "v1",
+        "lineColor": "#FF6600",
+        "bullet": "round",
+        "bulletBorderThickness": 1,
+        "hideBulletsCount": 30,
+        "title": "active users",
+        "valueField": "visits",
+		"fillAlphas": 0
+    }, {
+        "valueAxis": "v2",
+        "lineColor": "#e8f442",
+        "bullet": "square",
+        "bulletBorderThickness": 1,
+        "hideBulletsCount": 30,
+        "title": "total users",
+        "valueField": "hits",
+		"fillAlphas": 0
+    }],
+    "chartScrollbar": {},
+    "chartCursor": {
+        "cursorPosition": "mouse"
+    },
+    "categoryField": "date",
+    "categoryAxis": {
+        "parseDates": true,
+        "axisColor": "#DADADA",
+        "minorGridEnabled": true
+    },
+    "export": {
+    	"enabled": true,
+        "position": "bottom-right"
+     }
+});
+
+chart.addListener("dataUpdated", zoomChart);
+zoomChart();
+
+
+// generate some random data, quite different range
+function generateChartData() {
+
+  $(document).ready(function () {
+  $.ajax({
+  url: 'http://54.174.27.160:8080/analysis/snapshots/latest',
+    //url: 'https://jsonplaceholder.typicode.com/posts/1',
+    //data: { get_param: 'value' },
+      contentType:"application/x-www-form-urlencoded",
+      type: 'GET',
+      dataType: 'json',
+      crossDomain: true,
+      // async: false,
+    //    headers: {
+    //    'Access-Control-Allow-Origin' : 'http://localhost:8080/'
+    //  },
+    error: function() {
+       $('#info').html('<p>An error has occurred</p>');
+    },
+      success: function (data) {
+        console.log('data==',data);
+         console.log( "ready!" );
+        // chartData =data.dataProvider;
+        var userId = data.userId ;
+        var totalNumOfUsers= data.totalNumOfUsers ;
+        var totalNumOfAccounts=data.totalNumOfAccounts;
+        var totalNumOfThreads=data.totalNumOfThreads;
+        var totalNumOfFollowUpThreads=data.totalNumOfFollowUpThreads;
+        var totalNumOfUserModels=data.totalNumOfUserModels;
+        var totalValidRefreshTokens=data.totalValidRefreshTokens;
+        var avgMessagesPerThread=data.avgMessagesPerThread;
+        console.log("totalNumOfUsers>>>",totalNumOfUsers);
+        console.log("totalNumOfAccounts>>>",totalNumOfAccounts);
+        console.log("totalNumOfThreads>>>",totalNumOfThreads);
+        console.log("totalNumOfFollowUpThreads>>>",totalNumOfFollowUpThreads);
+        console.log("totalNumOfUserModels>>>",totalNumOfUserModels);
+        console.log("totalValidRefreshTokens>>>",totalValidRefreshTokens);
+        console.log("avgMessagesPerThread>>>",avgMessagesPerThread);
+      }
+
+  });
+   });
+
+    var chartData = [];
+    var firstDate = new Date();
+    firstDate.setDate(firstDate.getDate() - 100);
+
+    var totalNumOfUsers;
+    var totalNumOfAccounts;
+    var totalNumOfThreads;
+    var totalNumOfFollowUpThreads;
+    var totalNumOfUserModels;
+    var totalValidRefreshTokens;
+    var avgMessagesPerThread;
+
+    for (var i = 0; i < 100; i++) {
+        // we create date objects here. In your data, you can have date strings
+        // and then set format of your dates using chart.dataDateFormat property,
+        // however when possible, use date objects, as this will speed up chart rendering.
+        var newDate = new Date(firstDate);
+        newDate.setDate(newDate.getDate() + i);
+
+        var visits = Math.round(Math.sin(i * 5) * i);
+        var hits = Math.round(Math.random() * 80) + 500 + i * 3;
+        var views = Math.round(Math.random() * 6000) + i * 4;
+
+        chartData.push({
+            date: newDate,
+            visits: visits,
+            hits: hits,
+            views: views,
+
+            totalNumOfUsers: totalNumOfUsers,
+            totalNumOfAccounts: totalNumOfAccounts,
+            totalNumOfThreads: totalNumOfThreads,
+            totalNumOfFollowUpThreads: totalNumOfFollowUpThreads,
+            totalNumOfUserModels: totalNumOfUserModels,
+            totalValidRefreshTokens: totalValidRefreshTokens,
+            avgMessagesPerThread: avgMessagesPerThread
+        });
+    }
+    return chartData;
+}
+
+function zoomChart(){
+    chart.zoomToIndexes(chart.dataProvider.length - 20, chart.dataProvider.length - 1);
+}
